1fbf0958eb1810c944ac730e23228edc639cc35b
NishitDoshi <nishit.g.doshi.cg@gmail.com>
2026-04-23 23:59:59 +0530

    setup server

---
+++ Task-2/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const productRoutes = require("./routes/productRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", productRoutes);
+
+module.exports = app;
